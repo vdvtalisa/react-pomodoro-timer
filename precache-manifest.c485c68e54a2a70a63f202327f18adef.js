823e449a0cf5e9c103be63430744ac890877baa8
tallyvanv <talisa.vdv@gmail.com>
2020-04-22 13:22:52 +0200

    Updates

---
+++ precache-manifest.c485c68e54a2a70a63f202327f18adef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2af7c3252e3f6fb3f1faafff753a38",
+    "url": "/react-pomodoro-timer/index.html"
+  },
+  {
+    "revision": "c4d9d19e978fa62afdb2",
+    "url": "/react-pomodoro-timer/static/css/main.6d905cd6.chunk.css"
+  },
+  {
+    "revision": "7efe319ba20e614f39d8",
+    "url": "/react-pomodoro-timer/static/js/2.83e16d3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro-timer/static/js/2.83e16d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d9d19e978fa62afdb2",
+    "url": "/react-pomodoro-timer/static/js/main.5f1ecdfe.chunk.js"
+  },
+  {
+    "revision": "477571e7380a606a3528",
+    "url": "/react-pomodoro-timer/static/js/runtime-main.afb2d3d5.js"
+  }
+]);
